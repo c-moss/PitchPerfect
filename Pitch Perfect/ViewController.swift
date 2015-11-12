9e5000862a01340670d72eb97048e1587e2b8c50
cmoss <campbell.moss@fiserv.com>
2015-11-12 22:13:11 +1300

    End of lesson 3a: created navigation controller and second screen

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var recordingLabel: UILabel!
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        recordButton.enabled = true
+        stopButton.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func startRecordAudio(sender: UIButton) {
+        //TODO: Record user's voice
+        println("in recordAudio")
+        recordingLabel.hidden = false
+        recordButton.enabled = false
+        stopButton.hidden = false
+    }
+    
+    @IBAction func stopRecordAudio(sender: UIButton) {
+        recordingLabel.hidden = true
+        stopButton.hidden = true
+    }
 }
 
